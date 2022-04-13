f584b78de845fcdeb0376a6b6d1285a3fc60df9f
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-04-13 15:22:30 +0800

    🔬 use Figure API

--- draw.py
+++ draw.py
@@ -1,11 +1,9 @@
-import matplotlib
-import matplotlib.pyplot as plt
-
-matplotlib.use('Agg')
+from matplotlib.figure import Figure
 
 
 def save_img(name: str) -> str:
-    fig, ax = plt.subplots()
+    fig = Figure()
+    ax = fig.subplots()
     ax.plot([1, 2, 3, 4])
-    plt.savefig(f"{name}.jpg", format="jpg", bbox_inches="tight")
+    fig.savefig(f"{name}.jpg", format="jpg", bbox_inches="tight")
     return f"{name}.jpg"
