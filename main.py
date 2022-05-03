9276bfa1488c7d6c0b39f3a0727ff3e3687cafde
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-05-03 10:43:51 +0800

    🔊 log update_id/chat_id

--- main.py
+++ main.py
@@ -79,6 +79,8 @@ def checkout(update: Update, context: CallbackContext):
 
 
 def today(update: Update, context: CallbackContext):
+    print(f"update_id: {update.update_id}")
+    print(f"chat_id: {update.message.chat_id}")
     sheet = get_sheet()
     df = pd.DataFrame(sheet.get_all_records())
     today_str = dt.now().strftime("%Y-%m-%d")
